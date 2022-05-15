3b1cbe3e52daeef998ffe3f23167881c85b37f89
symaeng98 <symaeng98@naver.com>
2022-05-15 18:35:25 +0900

    복습

--- src/main/java/com/example/inflearnweb/controller/MemberController.java
+++ src/main/java/com/example/inflearnweb/controller/MemberController.java
@@ -31,9 +31,7 @@ public class MemberController {
     public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName());
-
         memberService.join(member);
-
         return "redirect:/"; //홈 화면으로 돌아감
     }
 
